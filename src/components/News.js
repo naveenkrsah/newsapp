58fc40b7342d33f0cd73cf6cfd7d2a7efde74e1d
naveenkrsah <125266678+naveenkrsah@users.noreply.github.com>
2023-06-11 23:37:22 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -42,46 +42,16 @@ export class News extends Component {
 
   }
   async componentDidMount(){
-    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9ef80a7ce8824b3995e4347524b8c596&pagesize=${this.props.pagesize}`;
-    // let data = await fetch(url);
-    // let parsedData = await data.json()
-    // console.log(parsedData);
-    // this.setState({articles: parsedData.articles ,totalresults: parsedData.totalResults})
     this.updateNews();
   }
 
 
   handleprevclick = async ()=>{
-    // console.log("prev");
-    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9ef80a7ce8824b3995e4347524b8c596&page=${this.state.page-1}&pagesize=${this.props.pagesize}`;
-    // this.setState({loading:true});
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-    // console.log(parsedData);
-    // this.setState({page: this.state.page-1,
-    //   articles: parsedData.articles,
-    //   loading:false
-    // });
     this.setState({page: this.state.page-1});
     this.updateNews();
   }
 
   handlenextclick = async ()=>{
-    // console.log("next");
-    // if(this.state.page +1 > Math.ceil(this.state.totalresults/this.props.pagesize)){
-
-    // }
-    // else{
-    //       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9ef80a7ce8824b3995e4347524b8c596&page= ${this.state.page+1}&pagesize=${this.props.pagesize}`;
-    //       this.setState({loading:true});
-    //       let data = await fetch(url);
-    //       let parsedData = await data.json();
-    //       console.log(parsedData);
-    //       this.setState({
-    //         page: this.state.page+1,
-    //         articles: parsedData.articles,
-    //         loading: false
-    //       });
     this.setState({page: this.state.page+1});
     this.updateNews();
   }
@@ -123,13 +93,6 @@ export class News extends Component {
           </div>
         </div>
           </InfiniteScroll>
-        {/* <div className="container my-4 d-flex justify-content-between ">
-        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handleprevclick}>&larr;Previous</button>
-        <button disabled={this.state.page +1 > Math.ceil(this.state.totalresults/this.props.pagesize)} type="button" className="btn btn-dark" onClick={this.handlenextclick}>Next&rarr;</button>
-        </div> */}
-       
-        
-      // </div>
       </>
     )
   }
